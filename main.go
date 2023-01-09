db2c11be4b82991a293f4c7290c16ff0b644b0dd
guoguangwu <guoguangwu@magic-shield.com>
2023-01-09 10:12:44 +0800

    增加漏扫功能

--- main.go
+++ main.go
@@ -2,22 +2,33 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/aquasecurity/trivy/pkg/commands/artifact"
 	flag2 "github.com/aquasecurity/trivy/pkg/flag"
+	"github.com/aquasecurity/trivy/pkg/types"
+	"github.com/aquasecurity/trivy/pkg/utils"
 )
 
 func main() {
 	var (
-		imgName string
+		srcName string
 	)
-	flag.StringVar(&imgName, "image", "", "镜像名")
+	flag.StringVar(&srcName, "src", "", "镜像名")
 	flag.Parse()
-	fmt.Println(imgName)
+	fmt.Println(srcName)
 	ctx := context.Background()
 	cliOpt := flag2.Options{}
-	cliOpt.Target = imgName
+	dbOpt := flag2.DBOptions{}
+	dbOpt.SkipDBUpdate = true
+	cliOpt.Target = srcName
+	cliOpt.OfflineScan = true
+	cliOpt.SecurityChecks = []string{"vuln"}
+	cliOpt.DBOptions = dbOpt
+	cliOpt.GlobalOptions.CacheDir = utils.DefaultCacheDir()
+	cliOpt.VulnType = []string{types.VulnTypeOS, types.VulnTypeLibrary}
+	fmt.Println(cliOpt.GlobalOptions.CacheDir)
 	r, err := artifact.NewRunner(ctx, cliOpt)
 	if err != nil {
 		fmt.Println("artifact.NewRunner err:", err.Error())
@@ -30,10 +41,7 @@ func main() {
 		fmt.Println("ScanImage err:", err.Error())
 		return
 	}
-	if report.Metadata.OS == nil {
-		fmt.Println("Do not has base image info: ", imgName)
-		return
-	}
-	fmt.Println("os :", report.Metadata.OS.Family)
+	data, _ := json.Marshal(report)
+	fmt.Println(string(data))
 }
 
